96166ec80b36f6faad864774ca78cd1ed03c6ec4
Vishnupriya <vpadhy3012@gmail.com>
2023-08-09 00:11:02 +0530

    clean-up members

--- MEMBERS/urls.py
+++ MEMBERS/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/members/', views.DSmembersOverview, name="DS-members-overview"),
-    path('api/members/current-members/', views.get_current_members, name="current_members"),
-    path('api/members/alumni/', views.get_alumni, name="alumni"),
-    path('api/members/all-members/<int:passout_yr>/', views.get_all_members, name="all-members"),
-    path('api/members/sophomores/', views.get_sophomores, name="sophomores"),
-    path('api/members/pre-final-years/', views.get_pre_final_years, name="pre-final-years"),
-    path('api/members/final-years/', views.get_final_years, name="final-years"),
+    path('', views.DSmembersOverview, name="DS-members-overview"),
+    path('current-members/', views.get_current_members, name="current_members"),
+    path('alumni/', views.get_alumni, name="alumni"),
+    path('all-members/<int:passout_yr>/', views.get_all_members, name="all-members"),
+    path('sophomores/', views.get_sophomores, name="sophomores"),
+    path('pre-final-years/', views.get_pre_final_years, name="pre-final-years"),
+    path('final-years/', views.get_final_years, name="final-years"),
     path('create_members/', views.members_form, name="create_members"),
   ]
